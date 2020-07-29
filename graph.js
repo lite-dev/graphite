f8db012462614efc6af41bdade3da285daa50274
lite-dev <jamzx1994@hotmail.com>
2020-07-29 06:49:53 -0400

    Refactor & Global Object
    Introduce Global object and refactor code.

--- graph.js
+++ graph.js
@@ -1,4 +1,12 @@
-class Graph {
+class __Global__ {
+	static graphs = new Map();
+
+	static temp_node = null;
+	static temp_drag = null;
+	static temp_drop = null;
+}
+
+class __Graph__ {
 	_element; _type;
 
 	types = {
@@ -6,19 +14,24 @@ class Graph {
 		Snap     : Snap,
 	};
 
-	nodes          = new Map();
-	tmp_link_nodes = [];
-	temp_node      = {
-		node    : null,
-		offsetX : null,
-		offsetY : null,
-	};
+	nodes           = new Map();
+	temp_link_nodes = [];
 
 	constructor(element, _default = `FreeForm`) {				
 		this.type    = _default;
 		this.element = element;
 
-		window.addEventListener('resize', event => this.canvas && this.drawNodes());
+		__Global__.graphs.set(element, this);
+	}
+
+	get element() {
+		return this._element;
+	}
+
+	set element(value) {
+		this._element = value;
+
+		this.addGraphEvents(value);
 	}
 
 	get type() {
@@ -31,7 +44,7 @@ class Graph {
 		if(has) {
 			this._type = value;
 	
-			this.types[value]();
+			this.types[value].call(this);
 		}
 	}
 
@@ -45,69 +58,23 @@ class Graph {
 		}
 	}
 
-	drawNodes() {
-		const element   = this.element;
-		const canvas    = this.canvas;
-		const nodes     = this.nodes.values();
-		const context   = canvas.getContext(`2d`);
-		const grid_size = this.grid_size;
-
-		canvas.width  = element.clientWidth;
-		canvas.height = element.clientHeight;
-
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		this.gridLines(grid_size);
-
-		for(const node of nodes) {
-			this.draw && this.draw(node);
-		}
-	}
-
-	addNodeEvents(element) {
-		const drag = element.getAttribute(`draggable`);
-
-		if(!Boolean(drag)) {
-			element.addEventListener(`dragstart`, (event) => {
-				this.getNodeEvent(event, element);
-			});
-
-			element.addEventListener(`click`, (event) => {
-				this.setActiveNodeEvent(event, element);
-			});
-		}
-
-		element.setAttribute(`draggable`, true);
-	}
-
-	addGraphEvents(element) {
-		element.addEventListener(`drop`, (event) => {
-			this.setNodeEvent(event);
-		});
-
-		element.addEventListener(`dragover`, (event) => event.preventDefault());
-	}
-
-	setNodeEvent(event) {
-		const node      = this.temp_node.node;
-		const rect      = this.element.getBoundingClientRect();
-		const x         = event.clientX - rect.left;
-		const y         = event.clientY - rect.top;
-
-		this.plot(node, x, y);
-		this.drawNodes();
+	plot(node) {
+		this.element.appendChild(node.element);
+		this.addNodeEvents(node.element);
+		this.setNode(node);
 	}
 
 	setActiveNodeEvent(event, element) {
 		const self  = this;
 		const node  = self.getNode(element);
-		const limit = self.tmp_link_nodes.length > 1;
+		const limit = self.temp_link_nodes.length > 1;
 
 		function push() {
-			self.tmp_link_nodes.push(node);
+			self.temp_link_nodes.push(node);
 		}
 
 		function pop() {
-			self.tmp_link_nodes.pop();
+			self.temp_link_nodes.pop();
 		}
 
 		if(node.active) {
@@ -126,12 +93,55 @@ class Graph {
 		node.active = node.active ? false : true;
 	}
 
+	addGraphEvents() {
+		this.element.addEventListener(`drop`, (event) => {
+			__Global__.temp_drop = event;
+	
+			this.setNodeProperties();
+			this.plot(__Global__.temp_node);
+			this.drawNodes();
+		});
+	
+		this.element.addEventListener(`dragover`, event => event.preventDefault());
+		window.addEventListener('resize', event => this.canvas && this.drawNodes());
+	}
+	
+	addNodeEvents(element) {
+		const drag = element.getAttribute(`draggable`);
+	
+		if(!Boolean(drag)) {
+			element.addEventListener(`dragstart`, (event) => {
+				__Global__.temp_drag = event;
+				__Global__.temp_node = this.getNode(element);
+			});
+	
+			element.addEventListener(`click`, (event) => {
+				this.setActiveNodeEvent(event, element);
+			});
+		}
+	
+		element.setAttribute(`draggable`, true);
+	}
+
+	setNodeProperties() {
+		const rect  = this.element.getBoundingClientRect();
+		const node  = __Global__.temp_node;
+		const x     = __Global__.temp_drop.clientX - rect.left;
+		const y     = __Global__.temp_drop.clientY - rect.top;
+
+		node.element.style.cssText = null;
+		node.graph                 = this;
+
+		node.x = x;
+		node.y = y;
+	}
+
 	register(data) {
 		Object.assign(this.types, data);
 	}
 }
 
-class Node {
+class __Node__ {
 	_element; _x; _y; _offset_x; _offset_y; _active; _grid_size;
 
 	parent_nodes = new Set();
@@ -226,191 +236,69 @@ class Node {
 	}
 }
 
-function FreeForm() {
-	const element = {
-		configurable : true,
-		enumerable   : true,
+class Graph extends __Graph__ {}
+class Node extends __Node__ {}
 
-		get() {
+function FreeForm() {
+	class FreeForm_Graph extends __Graph__ {
+		get element() {
 			return this._element;
-		},
+		}
 	
-		set(value) {
+		set element(value) {
 			value.classList.add(`__graph__`);
 	
-			this.addGraphEvents(value);
 			this._element = value;
 		}
-	};
+	}
 
-	const x = {
-		configurable : true,
-		enumerable   : true,
-
-		get() {
+	class FreeForm_Node extends __Node__ {
+		get x() {
 			return this._x;
-		},
-
-		set(value) {
+		}
+	
+		set x(value) {
 			const offset_x = this.offset_x || 0;
 	
 			this._x = value + this.width / 2;
 	
 			this.element.style.left = value ? `${value - offset_x}px` : null;
 		}
-	};
 
-	const y = {
-		configurable : true,
-		enumerable   : true,
-
-		get() {
+		get y() {
 			return this._y;
-		},
-
-		set(value) {
+		}
+	
+		set y(value) {
 			const offset_y = this.offset_y || 0;
-
+	
 			this._y = value;
 	
 			this.element.style.top  = value ? `${value - offset_y}px` : null;
 		}
-	};
+	}
 
-	const plot = {
-		configurable : true,
-		enumerable   : true,
-		writable     : true,
-		value        : function plot(node, x, y) {
-			node.element.style.cssText = null;
-	
-			node.x = x;
-			node.y = y;
-	
-			this.element.appendChild(node.element);
-			this.addNodeEvents(node.element);
-			this.setNode(node);
-		},
-	};
-
-	const get_node_event = {
-		configurable : true,
-		enumerable   : true,
-		writable     : true,
-		value        : function getNodeEvent(event, element, node = {}) {
-			node = this.getNode(element) || node;
-	
-			this.temp_node.node = node;
-	
-			node.offset_x = event.offsetX;
-			node.offset_y = event.offsetY;
-		},
-	};
-
-	Object.defineProperty(Graph.prototype, `element`, element);
-	Object.defineProperty(Graph.prototype, `plot`, plot);
-	Object.defineProperty(Graph.prototype, `getNodeEvent`, get_node_event);
-
-	Object.defineProperty(Node.prototype, `x`, x);
-	Object.defineProperty(Node.prototype, `y`, y);
+	Reflect.setPrototypeOf(Graph.prototype, FreeForm_Graph.prototype);
+	Reflect.setPrototypeOf(Node.prototype, FreeForm_Node.prototype);
 }
 
 function Snap() {
-	const element = {
-		configurable : true,
-		enumerable   : true,
-
-		get() {
+	class Snap_Graph extends __Graph__ {
+		get element() {
 			return this._element;
-		},
+		}
 	
-		set(value) {
+		set element(value) {
 			value.classList.add(`__grid__`);
 	
-			this.addGraphEvents(value);
-			this._element = value;
+			super.element = value;
 		}
-	};
 
-	const x = {
-		configurable : true,
-		enumerable   : true,
-
-		get() {
-			return this._x;
-		},
-
-		set(value) {
-			this._x = value;
-
-			const grid_size = this.grid_size;
-			const element   = this.element;
-			const coord_x   = this.coord_x;
-			const offset_x  = this.offset_x;
-
-			this._x = grid_size * (coord_x - 1) - offset_x;
-	
-			element.style.left            = `${-offset_x}px`;
-			element.style.gridColumnStart = coord_x;
-		}
-	};
-
-	const y = {
-		configurable : true,
-		enumerable   : true,
-
-		get() {
-			return this._y;
-		},
-
-		set(value) {
-			this._y = value;
-
-			const grid_size = this.grid_size;
-			const element   = this.element;
-			const coord_y   = this.coord_y;
-			const offset_y  = this.offset_y;
-
-			this._y = grid_size * (coord_y - 1) - offset_y;
-	
-			element.style.top          = `${-offset_y}px`;
-			element.style.gridRowStart = coord_y;
-		}
-	};
-
-	const coord_x = {
-		configurable : true,
-		enumerable   : true,
-
-		get() {
-			const x         = this.x;
-			const grid_size = this.grid_size;
-	
-			return Math.round(x / grid_size) + 1;
-		}
-	};
-
-	const coord_y = {
-		configurable : true,
-		enumerable   : true,
-
-		get() {
-			const y         = this.y;
-			const grid_size = this.grid_size;
-	
-			return Math.round(y / grid_size) + 1;
-		}
-	};
-
-	const canvas = {
-		configurable : true,
-		enumerable   : true,
-	
-		get() {
+		get canvas() {
 			return this._canvas;
-		},
+		}
 	
-		set(value) {
+		set canvas(value) {
 			value.classList.add(`__canvas__`);
 	
 			value.width  = value.clientWidth;
@@ -418,44 +306,29 @@ function Snap() {
 	
 			this._canvas = value;
 		}
-	};
 
-	const snap = {
-		configurable : true,
-		enumerable   : true,
-
-		get() {
+		get snap() {
 			return this._snap;
-		},
+		}
 	
-		set(value) {
+		set snap(value) {
 			const method = value ? `add` : `remove`;
 	
 			this._snap = value;
 	
 			this.element.classList[method](`__grid__`);
 		}
-	};
 
-	const grid_size = {
-		configurable : true,
-		enumerable   : true,
-
-		get() {
+		get grid_size() {
 			return this._grid_size;
-		},
+		}
 	
-		set(value) {
+		set grid_size(value) {
 			this._grid_size = value;
 			this.gridLines(value);
 		}
-	};
 
-	const grid_lines = {
-		configurable : true,
-		enumerable   : true,
-		writable     : true,
-		value        : function gridLines(size) {
+		gridLines(size) {
 			const element = this.element;
 			const canvas  = this.canvas;
 			const context = canvas.getContext(`2d`);
@@ -484,47 +357,27 @@ function Snap() {
 	
 			element.style.gridTemplateColumns = `repeat(${columns}, ${size}px) auto`;
 			element.style.gridTemplateRows    = `repeat(${rows}, ${size}px) auto`;
-		},
-	};
+		}
 
-	const get_node_event = {
-		configurable : true,
-		enumerable   : true,
-		writable     : true,
-		value        : function getNodeEvent(event, element, node = {}) {
-			node = this.getNode(element) || node;
-	
-			this.temp_node.node = node;
-	
-			node.offset_x = element.width / 2;
-			node.offset_y = element.height / 2;
-		},
-	};
-
-	const plot = {
-		configurable : true,
-		enumerable   : true,
-		writable     : true,
-		value        : function plot(node, x, y) {
+		drawNodes() {
+			const element   = this.element;
+			const canvas    = this.canvas;
+			const nodes     = this.nodes.values();
+			const context   = canvas.getContext(`2d`);
 			const grid_size = this.grid_size;
 	
-			node.element.style.cssText = null;
+			canvas.width  = element.clientWidth;
+			canvas.height = element.clientHeight;
 	
-			node.grid_size = grid_size;
-			node.x         = x;
-			node.y         = y;
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			this.gridLines(grid_size);
 	
-			this.element.appendChild(node.element);
-			this.addNodeEvents(node.element);
-			this.setNode(node);
-		},
-	};
+			for(const node of nodes) {
+				this.draw && this.draw(node);
+			}
+		}
 
-	const draw = {
-		configurable : true,
-		enumerable   : true,
-		writable     : true,
-		value        : function draw(node) {
+		draw(node) {
 			const main     = node;
 			const canvas   = this.canvas;
 			const children = Array.from(main.child_nodes.values());
@@ -580,51 +433,91 @@ function Snap() {
 
 			main.active = false;
 		}
-	}
 
-	const link = {
-		configurable : true,
-		enumerable   : true,
-		writable     : true,
-		value        : function link() {
+		link() {
 			const self      = this;
-			const from_node = self.tmp_link_nodes.shift();
-			const to_node   = self.tmp_link_nodes.shift();
+			const from_node = self.temp_link_nodes.shift();
+			const to_node   = self.temp_link_nodes.shift();
 	
 			from_node.child_nodes.add(to_node);
 			to_node.parent_nodes.add(from_node);
 
 			self.draw(from_node);
-		},
-	};
+		}
 
-	Object.defineProperty(Graph.prototype, `element`, element);
-	Object.defineProperty(Graph.prototype, `canvas`, canvas);
-	Object.defineProperty(Graph.prototype, `snap`, snap);
-	Object.defineProperty(Graph.prototype, `grid_size`, grid_size);
-	Object.defineProperty(Graph.prototype, `gridLines`, grid_lines);
-	Object.defineProperty(Graph.prototype, `getNodeEvent`, get_node_event);
-	Object.defineProperty(Graph.prototype, `plot`, plot);
-	Object.defineProperty(Graph.prototype, `link`, link);
-	Object.defineProperty(Graph.prototype, `draw`, draw);
+		setNodeProperties() {
+			__Global__.temp_node.grid_size = this.grid_size;
+			__Graph__.prototype.setNodeProperties.call(this);
+		}
+	}
 
-	Object.defineProperty(Node.prototype, `coord_x`, coord_x);
-	Object.defineProperty(Node.prototype, `coord_y`, coord_y);
-	Object.defineProperty(Node.prototype, `x`, x);
-	Object.defineProperty(Node.prototype, `y`, y);
+	class Snap_Node extends __Node__ {
+		get x() {
+			return this._x;
+		}
+	
+		set x(value) {
+			this._x = value;
+	
+			const grid_size = this.grid_size;
+			const element   = this.element;
+			const coord_x   = this.coord_x;
+			const offset_x  = this.offset_x;
+	
+			this._x = grid_size * (coord_x - 1) - offset_x;
+	
+			element.style.left            = `${-offset_x}px`;
+			element.style.gridColumnStart = coord_x;
+		}
+
+		get y() {
+			return this._y;
+		}
+
+		set y(value) {
+			this._y = value;
+
+			const grid_size = this.grid_size;
+			const element   = this.element;
+			const coord_y   = this.coord_y;
+			const offset_y  = this.offset_y;
+
+			this._y = grid_size * (coord_y - 1) - offset_y;
+	
+			element.style.top          = `${-offset_y}px`;
+			element.style.gridRowStart = coord_y;
+		}
+
+		get coord_x() {
+			const x         = this.x;
+			const grid_size = this.grid_size;
+	
+			return Math.round(x / grid_size) + 1;
+		}
+
+		get coord_y() {
+			const y         = this.y;
+			const grid_size = this.grid_size;
+	
+			return Math.round(y / grid_size) + 1;
+		}
+	}
+
+	Reflect.setPrototypeOf(Graph.prototype, Snap_Graph.prototype);
+	Reflect.setPrototypeOf(Node.prototype, Snap_Node.prototype);
 }
 
 window.addEventListener(`DOMContentLoaded`, (event) => {
 	document.addEventListener(`__node_init__`, (event) => {
 		const detail   = event.detail || {};
-		const graph    = detail.graph;
 		const element  = event.target;
 		const internal = document.createElement(`div`);
 		
 		element.addEventListener(`dragstart`, (event) => {
 			const node = detail.init.call(element) || (new Node(internal));
-
-			graph.getNodeEvent(event, node.element, node);
+			
+			__Global__.temp_drag = event;
+			__Global__.temp_node = node;
 		});
 
 		element.setAttribute(`draggable`, true);
